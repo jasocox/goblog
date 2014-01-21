2a05ed55faa33d0cfbe735c7f86dd67353cc8edb
Jason Cox <JasonWilburCox@gmail.com>
2014-01-21 17:20:53 -0500

    Starting the webserver code

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
 
 func main() {
-  fmt.Println("Blog goes here")
+  fmt.Println("Starting")
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Blogs")
+  })
+
+  err := http.ListenAndServe(":2001", nil)
+  if err != nil {
+    fmt.Println("Something broke...")
+    fmt.Println(err.Error())
+  }
+
+  fmt.Println("Stopping")
 }
