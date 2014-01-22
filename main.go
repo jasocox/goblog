21cd3acf5944a6a1624de7db191f7afcca255efb
Jason Cox <JasonWilburCox@gmail.com>
2014-01-22 12:46:13 -0500

    go fmt and fixing a bag log statement

--- main.go
+++ main.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-  "fmt"
-  "html"
-  "net/http"
-  l4g "code.google.com/p/log4go"
+	l4g "code.google.com/p/log4go"
+	"fmt"
+	"html"
+	"net/http"
 )
 
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
-  l4g.Trace("Handling request for %s", html.EscapeString(r.URL.Path))
+	l4g.Trace("Handling request for %s", html.EscapeString(r.URL.Path))
 
-  fmt.Fprintln(w, "Blogs")
+	fmt.Fprintln(w, "Blogs")
 }
 
 func main() {
-  l4g.Trace("Starting")
+	l4g.Trace("Starting")
 
-  http.HandleFunc("/", HandleRoot)
+	http.HandleFunc("/", HandleRoot)
 
-  err := http.ListenAndServe(":2001", nil)
-  if err != nil {
-    fmt.Println("Something broke...")
-    fmt.Println(err.Error())
-  }
+	err := http.ListenAndServe(":2001", nil)
+	if err != nil {
+		l4g.Error("Problem with http server: %s", err)
+	}
 
-  l4g.Trace("Stopping")
+	l4g.Trace("Stopping")
 }
