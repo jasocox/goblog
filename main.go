f1e91ccc336f66671a51200940304f99f94105b0
Jason Cox <JasonWilburCox@gmail.com>
2014-01-24 09:49:35 -0500

    Adding command line parsing for location of blog files

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	l4g "code.google.com/p/log4go"
+	"flag"
 	"fmt"
 	"html"
 	"net/http"
 )
+
+var blog_dir = flag.String("b", "", "directory where blogs a stored")
 
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	l4g.Trace("Handling request for %s", html.EscapeString(r.URL.Path))
@@ -15,6 +18,12 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	l4g.Trace("Starting")
+
+	flag.Parse()
+
+	if *blog_dir == "" {
+		l4g.Error("Must specify a directory where blogs are stored")
+	}
 
 	http.HandleFunc("/", HandleRoot)
 
