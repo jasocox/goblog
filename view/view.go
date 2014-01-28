5de940d8e10e3e0a48622c01fd4e7d26bbe54576
Jason Cox <JasonWilburCox@gmail.com>
2014-01-28 11:44:35 -0500

    Setting up header and footer

--- view/view.go
+++ view/view.go
@@ -8,9 +8,19 @@ import (
 	"net/http"
 )
 
-var templates = template.Must(template.ParseFiles("view/blog.html"))
+var (
+	views     = "view/"
+	header    = "header.html"
+	footer    = "footer.html"
+	blog      = "blog.html"
+	templates = template.Must(template.ParseFiles(
+		views+header,
+		views+footer,
+		views+blog,
+	))
+)
 
-func Blog(w http.ResponseWriter, b *reader.Blog) {
+func Blog(w http.ResponseWriter, b *reader.Blog) (err error) {
 	if b == nil {
 		l4g.Info("Requested blog that does not exist")
 		fmt.Fprintln(w, "NOT FOUND! :D")
@@ -19,5 +29,30 @@ func Blog(w http.ResponseWriter, b *reader.Blog) {
 
 	l4g.Trace("Displaying blog: " + b.Title)
 
-	templates.ExecuteTemplate(w, "blog.html", b)
+	l4g.Trace("Rendering the header")
+	err = renderHeader(w)
+	if err == nil {
+		l4g.Trace("Rendering the blog")
+		err = templates.ExecuteTemplate(w, blog, b)
+	}
+	if err == nil {
+		l4g.Trace("Rendering the footer")
+		err = renderFooter(w)
+	}
+
+	if err != nil {
+		l4g.Error("Problems rendering template: " + err.Error())
+		fmt.Fprintln(w, "Nope! "+err.Error())
+	}
+
+	l4g.Trace("Done rendering")
+	return
+}
+
+func renderHeader(w http.ResponseWriter) error {
+	return templates.ExecuteTemplate(w, header, nil)
+}
+
+func renderFooter(w http.ResponseWriter) error {
+	return templates.ExecuteTemplate(w, footer, nil)
 }
