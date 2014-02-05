840c62beeb1e03ac7abfe9b8481a11bfbd956334
Jason Cox <JasonWilburCox@gmail.com>
2014-02-05 15:43:18 -0500

    Adding a real selenium test

--- tests/functional/home.rb
+++ tests/functional/home.rb
@@ -1,5 +1,19 @@
 require "./tests/selenium"
-$driver.navigate.to "http://localhost:2002/"
 
-sleep 1
+begin
+  $driver.navigate.to "http://localhost:2002/"
+
+  element = $driver.find_element(:class => "navbar")
+  links = element.find_elements(:tag_name, "a")
+  if "About Me" != links[0].text
+    raise "About Me link missing"
+  end
+  if "Contact Me" != links[1].text
+    raise "Contact Me link missing"
+  end
+rescue Exception => e
+  puts "Tests failed!"
+  puts e
+end
+
 $driver.quit
--- tests/selenium.rb
+++ tests/selenium.rb
@@ -1,3 +1,4 @@
 require "selenium-webdriver"
 
 $driver = Selenium::WebDriver.for :firefox
+$wait = Selenium::WebDriver::Wait.new(:timeout => 3)
